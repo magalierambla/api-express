3ceb6fa84d92464645b02bb0bd4c7ce3b47e85a2
magalierambla <magalierambla@users.noreply.github.com>
2020-10-31 17:35:31 +0100

    feat(api-express): adding get, create and delete endpoints

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express'); 
 const app = express();
 
+app.use(express.json());
+
 const mockDb = [
     { id: 1, title: 'première tache', completed: false },
     { id: 2, title: '2e tache', completed: true },
@@ -10,12 +12,13 @@ const mockDb = [
     { id: 6, title: '6e tache', completed: true }
 ]
 
-app.get('/', (resquest, response) => {
+app.get('/', (request, response) => {
     response.send('Coucou Express!');
 })
 
-app.get('/api/todos', (resquest, response) => {
-    const todos = mockDb;
+app.get('/api/todos', (request, response) => {
+   
+    const todos = mockDb.filter(todo => !todo.completed);
 
     if(!todos) {
         return response.status(400).send('Error cannot find your todos!');
@@ -24,5 +27,72 @@ app.get('/api/todos', (resquest, response) => {
     //falsy/trusy à voir 
 })
 
-app.listen(3000, () => console.log('listening server on port 3000...'));
+app.get('/api/todos/completed', (request, response) => {
+   
+    const todosCompleted = mockDb.filter(todo => todo.completed);
 
+    if(!todosCompleted) {
+        return response.status(400).send('Error cannot find your todos!');
+    }
+    return response.send(todosCompleted);
+    //falsy/trusy à voir 
+})
+
+app.get('/api/todos/:id', (request, response) => {
+
+    const foundTodo = mockDb.filter((todo, index) => {
+        return todo.id === parseInt(request.params.id);
+    });
+
+    if(!foundTodo) {
+        return response.status(400).send('Error cannot find your todo!');
+    }
+    return response.send(foundTodo);
+    //falsy/trusy à voir 
+})
+
+app.post('/api/todos', (request, response) => {
+
+    const todoNew = {
+        id: mockDb.length+1, 
+        title: request.body.title,
+        completed: false
+    };
+
+    mockDb.push(todoNew);
+
+    return response.send({
+        message:'New todo created!',
+        mockDb
+    });
+    //falsy/trusy à voir 
+})
+
+app.delete('/api/todos/:id', (request, response) => {
+
+    const todos = mockDb.filter(todo => parseInt(request.params.id) !== todo.id);
+
+    return response.send({
+        message: `Todo #${request.params.id} deleted!`,
+        todos
+    });
+    //falsy/trusy à voir 
+})
+
+
+app.patch('/api/todos/:id', (request, response) => {
+//1. trouver la todo req.params.id dans mockDb
+//2. if request.body.title modifier le title
+//3. if request.body.completed remplacer l'actuel todo.completed par le request.body.completed
+
+
+    const todos = mockDb.filter(todo => parseInt(request.params.id) !== todo.id);
+
+    return response.send({
+        message: `Todo #${request.params.id} deleted!`,
+        todos
+    });
+    //falsy/trusy à voir 
+})
+
+app.listen(3000, () => console.log('listening server on port 3000...'));
